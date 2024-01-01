7c27e2ee4fdcad5b4ebb462307217eecab849941
1 <menathlakvindub@gmail.com>
2024-01-01 22:07:35 +0530

    added button, jsx/css

---
+++ JSX  CSS  Simple Button/button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./button.css";
+
+return function Button(props) {
+        <button 
+            id={props.buttonName} 
+            type="submit" 
+            className="button"
+            onClick={Home}
+            >
+                <div className="sign-up-name">Sign Up</div>
+        </button>
+}
+
+export default Button;
